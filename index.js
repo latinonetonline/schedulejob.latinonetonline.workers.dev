74ce566a39b3b66678e717ad8f5b50285ac256d0
lautaro <lautaroecarro@gmail.com>
2020-07-12 01:05:12 -0300

    fix all

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-/**
- * Example of how router can be used in an application
- *  */
 addEventListener('fetch', event => {
-    return event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
 async function handleRequest(request) {
 
     const url = new URL(request.url);
@@ -17,13 +18,13 @@ async function handleRequest(request) {
 
     console.log("Job id received: " + job)
 
-    console.log(GITHUB_TOKEN);
     const init = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/vnd.github.v3+json',
-            'Authorization': `Bearer ${GITHUB_TOKEN}`
+            'User-Agent': 'PostmanRuntime/7.26.1',
+            'Authorization': 'Bearer ' + GITHUB_TOKEN
         },
         body: JSON.stringify({
             "event_type": "dispatch-workflow-job",
@@ -35,38 +36,13 @@ async function handleRequest(request) {
     }
     console.log("Send Fetch");
 
-    const fetchResponse = await fetch(url, init)
-
-    console.log("Status Code: " + fetchResponse.status);
+    const fetchResponse = await fetch("https://api.github.com/repos/latinonetonline/LatinoNETOnline.ScheduleJob/dispatches", init)
 
     return new Response('{}', {
-        status: fetchResponse.status,
+        status: fetchResponse.status == 204 ? 200 : 400,
         statusText: fetchResponse.statusText,
         headers: {
             'content-type': 'application/json',
         },
     })
 }
-
-/**
- * gatherResponse awaits and returns a response body as a string.
- * Use await gatherResponse(..) in an async function to get the response body
- * @param {Response} response
- */
-async function gatherResponse(response) {
-    const { headers } = response
-    const contentType = headers.get('content-type') || ''
-    if (contentType.includes('application/json')) {
-        return JSON.stringify(await response.json())
-    } else if (contentType.includes('application/text')) {
-        return await response.text()
-    } else if (contentType.includes('text/html')) {
-        return await response.text()
-    } else {
-        return await response.text()
-    }
-}
-
-
-const url = 'https://api.github.com/repos/latinonetonline/LatinoNETOnline.ScheduleJob/dispatches';
-
